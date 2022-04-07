d0007e21829ae27d223cbe1ff90f30d7abb91368
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-04-06 20:26:20 -0500

    added validation middleware and CORS

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const { deleteProduct } = require("./repository/products-repository");
+const cors = require("cors");
 const repoContext = require("./repository/repository-wrapper");
+const productValidate = require("./middleware/product-validation");
+const productLogger = require("./middleware/product-logger");
 const app = express();
 
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+
 
 // Endpoints
 app.get("/api/products", (req, res) => {
@@ -21,7 +26,7 @@ app.get("/api/products/:id", (req, res) => {
     return res.send(product);
 })
 
-app.post("/api/products", (req,res) => {
+app.post("/api/products",[productLogger,productValidate], (req,res) => {
     const newProduct = req.body;
     const addedProduct = repoContext.products.createProduct(newProduct);
     return res.status(201).send(addedProduct);
