c58929f3fbf535343579d27e88778ab16532ea99
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-04-06 19:25:39 -0500

    added GET(by id) and POST methods

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const repoContext = require("./repository/repository-wrapper");
 const app = express();
 
 
 //Middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // Endpoints
-app.get("/products", (req, res) => {
+app.get("/api/products", (req, res) => {
     console.log(req.headers);
-    res.send("This is a response!");
+    const products = repoContext.products.findAllProducts();
+    return res.send(products);
+})
+
+app.get("/api/products/:id", (req, res) => {
+    const id = req.params.id;
+    const product = repoContext.products.findProductById(id);
+    return res.send(product);
+})
+
+app.post("/api/products", (req,res) => {
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return res.status(201).send(addedProduct);
 })
 
 // Starting a server
